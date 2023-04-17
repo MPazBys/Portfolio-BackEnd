9b1c3bdb3ea396c4143e4720183557da6b8c786b
MPazBys <pazbys25@gmail.com>
2023-04-17 15:04:49 -0300

    CargaInicial

---
+++ MPB/src/test/java/com/portfolio/MPB/MpbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.MPB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
